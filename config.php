8e9ee5e6007d4ee08d85dabd6cec4f1d9a73d4da
Joshua Hürzeler <joshua.caramelli@gmail.com>
2019-09-21 20:03:04 +0200

    Little typo in config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $cfg_client_id          = $_ENV["NANOGP2_CLIENT_ID"];
-$cfg_client_secret      = $_ENV["NANOGP2_CLIENT_SECRED"];
+$cfg_client_secret      = $_ENV["NANOGP2_CLIENT_SECRET"];
 $cfg_max_accounts       = $_ENV["NANOGP2_MAX_ACCOUNTS"];
 $cfg_log                = true;
 $albums_filter          = explode(',',$_ENV["NANOGP2_ALBUMS_FILTER"]);
