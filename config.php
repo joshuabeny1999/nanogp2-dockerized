5dab0714915c34ba94cc9060a25499d611d71ea4
Joshua Hürzeler <joshua.caramelli@gmail.com>
2019-09-21 20:34:37 +0200

    Disalbe log in textfile

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 $cfg_client_id          = $_ENV["NANOGP2_CLIENT_ID"];
 $cfg_client_secret      = $_ENV["NANOGP2_CLIENT_SECRET"];
 $cfg_max_accounts       = $_ENV["NANOGP2_MAX_ACCOUNTS"];
-$cfg_log                = true;
+$cfg_log                = false;
 $albums_filter          = explode(',',$_ENV["NANOGP2_ALBUMS_FILTER"]);
 ?>
